3c80a5e9a0ff962b91c035b9dd68210b522de02e
David Parra Perez <david.parra@outlook.com>
2018-08-24 12:07:50 +0200

    test for istio

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@ cd /usr/src/app/conf
 rm -rf /usr/src/app/conf/*
 rm -f /usr/src/app/conf/.env
 STR=$'#!/bin/bash\n'
-echo "$STR" > .env
+#echo "$STR" > .env
 
-wget $GLOBALCONF -O env-global.sh 
+#wget $GLOBALCONF -O env-global.sh 
 
-wget $SYSTEMCONF -O env-project.sh
+#wget $SYSTEMCONF -O env-project.sh
 
-cat env-global.sh env-project.sh > .env
+#cat env-global.sh env-project.sh > .env
 
 nginx -g "daemon off;"
